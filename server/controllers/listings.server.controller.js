d041525187b46f1288f4a63f90aef57948bd96bc
Jeff Hornbeck <jthornbeck@ufl.edu>
2017-09-16 18:31:27 -0400

    finished except diagram

--- server/controllers/listings.server.controller.js
+++ server/controllers/listings.server.controller.js
@@ -1,14 +1,14 @@
 
 /* Dependencies */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Listing = require('../models/listings.server.model.js');
 
 /*
   In this file, you should use Mongoose queries in order to retrieve/add/remove/update listings.
-  On an error you should send a 404 status code, as well as the error message. 
+  On an error you should send a 404 status code, as well as the error message.
   On success (aka no error), you should send the listing(s) as JSON in the response.
 
-  HINT: if you are struggling with implementing these functions, refer back to this tutorial 
+  HINT: if you are struggling with implementing these functions, refer back to this tutorial
   from assignment 3 https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
  */
 
@@ -21,7 +21,7 @@ exports.create = function(req, res) {
   /* save the coordinates (located in req.results if there is an address property) */
   if(req.results) {
     listing.coordinates = {
-      latitude: req.results.lat, 
+      latitude: req.results.lat,
       longitude: req.results.lng
     };
   }
@@ -50,6 +50,22 @@ exports.update = function(req, res) {
   /* Replace the article's properties with the new properties found in req.body */
   /* save the coordinates (located in req.results if there is an address property) */
   /* Save the article */
+
+  listing.name = req.body.name;
+  listing.code = req.body.code;
+  listing.address = req.body.address;
+  
+  if(req.results){
+    listing.coordinates.latitude = req.results.latitude;
+    listing.coordinates.longitude = req.results.longitude;
+  }
+
+  listing.save(function(err) {
+    if(err) res.status(400).send(err);
+    else {
+      res.json(listing);
+    }
+  });
 };
 
 /* Delete a listing */
@@ -57,18 +73,30 @@ exports.delete = function(req, res) {
   var listing = req.listing;
 
   /* Remove the article */
+  listing.remove(function(err,listing){
+    if (err) res.status(400).send(err);
+    else{
+      res.end();
+    }
+  })
 };
 
 /* Retreive all the directory listings, sorted alphabetically by listing code */
 exports.list = function(req, res) {
   /* Your code here */
+  Listing.find().sort('code').exec(function(err,listing){
+    if (err) res.status(400).send(err);
+    else {
+      res.json(listing);
+    }
+  })
 };
 
-/* 
-  Middleware: find a listing by its ID, then pass it to the next request handler. 
+/*
+  Middleware: find a listing by its ID, then pass it to the next request handler.
 
-  HINT: Find the listing using a mongoose query, 
-        bind it to the request object as the property 'listing', 
+  HINT: Find the listing using a mongoose query,
+        bind it to the request object as the property 'listing',
         then finally call next
  */
 exports.listingByID = function(req, res, next, id) {
